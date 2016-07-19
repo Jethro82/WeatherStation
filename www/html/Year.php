72dc7d358062ca46eec361d8a110f206ab318dba
Jethro82 <admin@fransen.ca>
2016-07-19 14:59:06 -0500

    Update Year.php

--- www/html/Year.php
+++ www/html/Year.php
@@ -4,7 +4,7 @@
 	echo $Year.'</Title>';
 	include '../Menu.htm';
 	echo 'For '.$Year.'<br>';
-	echo '<table width=80%><td>Year<td>Average<td>High<td>Low<tr>';
+	echo '<table width=80%><td>Month Name<td>Average<td>High<td>Low<tr>';
 	$GroupBy='MonthName';
 	$Condition='ReadingType="A" or ReadingType="R"';
 	$url='Month.php?y='.$Year.'&m=';
