0becc08276136c5336bbd0d866b82f6f2fe37c98
Jethro82 <admin@fransen.ca>
2016-07-19 17:16:21 -0500

    Update WS.sql

--- WS.sql
+++ WS.sql
@@ -1,6 +1,7 @@
 Create Database WeatherStation;
 use WeatherStation
 Create Table tblTemp(Temp Decimal(3,1), Time TimeStamp default now(), ReadingType char(1) not null);
+Create Index indxTime on tblTemp(Time);
 create user WeatherStationFE identified by `WSFE1';
 grant select on tblTemp to WeatherStationFE;
 create user WSRecorder identified by 'WSR1';
